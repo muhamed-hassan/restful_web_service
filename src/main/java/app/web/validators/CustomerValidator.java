d9f4340e259c829a8ce79ccfc8235759e0eb83f1
muhamed-hassan <mohamed.hassan.work@gmail.com>
2026-03-24 07:20:39 +0200

    code refactoring

--- src/main/java/app/web/validators/CustomerValidator.java
+++ src/main/java/app/web/validators/CustomerValidator.java
@@ -12,34 +12,34 @@ import app.web.models.CustomerUpdateModel;
 public class CustomerValidator {
 	
 	private String nameField = "name";
-	private String nameFieldIsRequiredMsg = "Name is required";
+	private String nameFieldIsRequiredMsg = "Name is required.";
 	private String nameFieldValidationRegex = "[a-zA-Z\\ ]{5,250}";
-	private String nameFieldIsInvalidMsg = "Name contains invalid characters and shall contain letters only with maximum of 250";	
+	private String nameFieldIsInvalidMsg = "Name contains invalid characters which shall contain letters with minimum of 5 characters and maximum of 250 characters.";	
 	
 	private String nationalIdField = "nationalId";
-	private String nationalIdFieldIsRequiredMsg = "National ID is required";
+	private String nationalIdFieldIsRequiredMsg = "National ID is required.";
 	private String nationalIdFieldValidationRegex = "[0-9]{14}";
-	private String nationalIdFieldIsInvalidMsg = "National ID contains invalid characters and shall contain 14 digits only";
+	private String nationalIdFieldIsInvalidMsg = "National ID contains invalid characters which shall contain 14 digits only.";
 	
 	private String dateOfBirthField = "dob";
-	private String dateOfBirthFieldIsRequiredMsg = "Date of birth is required";
+	private String dateOfBirthFieldIsRequiredMsg = "Date of birth is required.";
 	private String dateOfBirthFieldValidationRegex = "((19[7-9][0-9])|(2[0-9][0-9][0-9]))-(0[1-9]|1[0-2])-(0[1-9]|([1-2][0-9])|3[0-1])";
-	private String dateOfBirthFieldIsInvalidMsg = "Date of birth contains invalid characters and shall be in the form of yyyy-MM-dd";
+	private String dateOfBirthFieldIsInvalidMsg = "Date of birth contains invalid characters which shall be in the form of yyyy-MM-dd.";
 	
 	private String mobileField = "mobile";
-	private String mobileFieldIsRequiredMsg = "Mobile is required";
+	private String mobileFieldIsRequiredMsg = "Mobile is required.";
 	private String mobileFieldValidationRegex = "(07)[0-9]{9}";
-	private String mobileFieldIsInvalidMsg = "Mobile contains invalid characters and shall be in this form 07123456789";
+	private String mobileFieldIsInvalidMsg = "Mobile contains invalid characters which shall be in the form of 07123456789.";
 	
 	private String emailField = "email";
-	private String emailFieldIsRequiredMsg = "Email is required";
+	private String emailFieldIsRequiredMsg = "Email is required.";
 	private String emailFieldValidationRegex = "[a-zA-Z0-9_\\.]+(\\@)[a-zA-Z0-9]+(\\.)[a-zA-Z]+";
-	private String emailFieldIsInvalidMsg = "Email format is invalid";
+	private String emailFieldIsInvalidMsg = "Email format is invalid.";
 	
 	private String mailingAddressField = "mailingAddress";
-	private String mailingAddressFieldIsRequiredMsg = "Mailing address is required";
-	private String mailingAddressFieldValidationRegex = "[a-zA-Z0-9-,\\ ]{15,250}";
-	private String mailingAddressFieldIsInvalidMsg = "Mailing address contains invalid characters and shall contain alphanumeric characters only with maximum of 250";
+	private String mailingAddressFieldIsRequiredMsg = "Mailing address is required.";
+	private String mailingAddressFieldValidationRegex = "[a-zA-Z0-9-,\\ ]{15,500}";
+	private String mailingAddressFieldIsInvalidMsg = "Mailing address contains invalid characters which shall contain alphanumeric characters with minimum of 15 characters and maximum of 500 characters.";
 	
 	
 	public void validateCustomerCreateModel(CustomerCreateModel customerCreateModel) {
